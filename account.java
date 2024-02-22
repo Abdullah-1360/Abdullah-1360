86ee9f4129b495c94cd04608a1710f1e01bf446a
Abdullah-1360 <124857916+Abdullah-1360@users.noreply.github.com>
2024-02-22 09:35:10 +0500

    account.java

--- account.java
+++ account.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package account;
 
-/**
- *
- * @author riphah
- */
 public class account {
     String name;
     double balance=0;
