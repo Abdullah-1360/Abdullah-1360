ed9602a1b85ba1c9628fc1060fe08b92b0c87a47
Abdullah-1360 <124857916+Abdullah-1360@users.noreply.github.com>
2024-02-22 09:37:54 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
 import account.account;
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         account acc= new account();
         acc.display();
